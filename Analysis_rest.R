7eae6c95ee27afb26115e4445f95ed7b1721a93c
ralitsa-k <44056292+ralitsa-k@users.noreply.github.com>
2022-08-17 11:42:23 +0100

    update analysis

--- Analysis_rest.R
+++ Analysis_rest.R
@@ -1,5 +1,6 @@
 
 library(tidyverse)
+library(rstatix)
 detach(package:plyr)
 
 colors_a = c('#B0D0D3', '#C08497','#F7AF9D')
@@ -11,12 +12,59 @@ groups_long = groups %>%
 
 
 # Affective state --------------------------
-dat_affect <- read_csv('dat_affect.csv')
+dat_affect <- read_csv('dat_affect.csv') %>%
+  dplyr::select(Participant_Public_ID, Task_Name, response) %>%
+  separate(Task_Name, into = c('task', 'block'), -1) %>%
+  mutate(block = ifelse(block == 1, 'first', 
+                        ifelse(block==2, 'second', 'third'))) %>%
+  dplyr::select(-task) %>%
+  rename('id' = 'Participant_Public_ID') %>%
+  distinct()
+
+dat_affect_soc = dat_affect %>%
+  inner_join(groups_long)
+
+ggplot(dat_affect_soc, aes(x = type, y = response)) +
+  geom_boxplot()
+ggplot(dat_affect_soc, aes(x = block, y = response)) +
+  geom_boxplot()
+ggplot(dat_affect_soc, aes(x = block, y = response, fill = type)) +
+  geom_boxplot()
+
+  
+res.aov <- anova_test(data = dat_affect_soc, dv = response, wid = id, within = type)
+get_anova_table(res.aov)
+plot(mod_aff)
+
+pwc <- dat_affect_soc %>%
+  pairwise_t_test(
+    response ~ type, paired = TRUE,
+    p.adjust.method = "bonferroni"
+  )
+pwc
+
 
 
 
 # attributes/affiliation -----------------------
-dat_affil <- read_csv('dat_affil.csv')
+dat_affil <- read_csv('dat_affil.csv') 
+  dplyr::select(Participant_Public_ID, Task_Name, response) %>%
+  separate(Task_Name, into = c('task', 'block'), -1) %>%
+  mutate(block = ifelse(block == 1, 'first', 
+                        ifelse(block==2, 'second', 'third'))) %>%
+  dplyr::select(-task) %>%
+  rename('id' = 'Participant_Public_ID') %>%
+  distinct()
+
+dat_affect_soc = dat_affect %>%
+  inner_join(groups_long)
+
+ggplot(dat_affect_soc, aes(x = type, y = response)) +
+  geom_boxplot()
+ggplot(dat_affect_soc, aes(x = block, y = response)) +
+  geom_boxplot()
+ggplot(dat_affect_soc, aes(x = block, y = response, fill = type)) +
+  geom_boxplot()
 
 
 
